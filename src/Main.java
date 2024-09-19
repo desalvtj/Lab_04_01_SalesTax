2f6577fd169a19b56b83b3f8c1bc39e2b7a259b4
desalvtj <desalvtj@mail.uc.edu>
2024-09-19 14:57:20 -0400

    Final and Complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        double purchasePrice = 31.47;
+        double salesTax = 0;
+        final double TAX_RATE = .05;
+
+        salesTax = purchasePrice * TAX_RATE;
+        System.out.println("The sales tax on a purchase of " + purchasePrice + " is $" + salesTax);
+
+
+
     }
 }
